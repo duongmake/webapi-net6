e21250c28f6be9b77c96711673963568d23cbcb1
duongmake <duongmake3@gmail.com>
2024-01-19 20:45:51 +0700

    [NetCore6WebAPI] - 01: Khởi tạo dự án WebAPI trên nền tảng .NET Core 6

---
+++ MyApiNetCore6/MyApiNetCore6/Data/BookStoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyApiNetCore6.Data
+{
+    public class BookStoreContext: DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options) { }
+
+        #region DBSet
+        public DbSet<Book>? Books { get; set;}
+        #endregion
+
+    }
+}
